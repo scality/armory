eabe0ad72ba4c31adac542dc7868aaeb06923c9a
Vianney Rancurel <vianney.rancurel@gmail.com>
2022-02-22 12:32:00 -0800

    ft: invalid refs in index.js

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ module.exports = {
     storage: {
         metadata: {
             MetadataFileServer:
-            require('./lib/storage/metadata/file/MetadataFileServer'),
+            require('./metadata/file/MetadataFileServer'),
             MetadataFileClient:
-            require('./lib/storage/metadata/file/MetadataFileClient'),
+            require('./metadata/file/MetadataFileClient'),
             LogConsumer:
-            require('./lib/storage/metadata/bucketclient/LogConsumer'),
+            require('./metadata/bucketclient/LogConsumer'),
         },
         data: {
             file: {
                 DataFileStore:
-                require('./lib/storage/data/file/DataFileStore'),
+                require('./data/file/DataFileStore'),
             },
         },
-        utils: require('./lib/storage/utils'),
+        utils: require('./utils'),
     },
 };
